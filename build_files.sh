c2cd24ab6da93efa19d14a2841723c3ae198c102
Siddhesh Chavan <siddhesh26.chavan@gmail.com>
2025-02-12 09:53:23 +0530

    changes made for deployment

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
+# filepath: D:/TY Project/Auctions-Django-Project-master/build_files.sh
+#!/bin/bash
 pip install -r requirements.txt
-python3.9 manage.py collectstatic 
+python3.9 manage.py collectstatic --noinput
